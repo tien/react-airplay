d82c931d0eaf905d6b6d66711218f33e18372a5b
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-03-04 19:44:39 +1300

    Clean up inconsistency

--- ios/modules/RAExternalPlaybackAvailabilityContext.swift
+++ ios/modules/RAExternalPlaybackAvailabilityContext.swift
@@ -4,11 +4,11 @@ import AVKit
 @objc(RAExternalPlaybackAvailabilityContext)
 class RAExternalPlaybackAvailabilityContext: RAEventEmitter {
     private let avRouteDetector = AVRouteDetector()
-    
+
     override func startObserving() {
         super.startObserving()
         self.avRouteDetector.isRouteDetectionEnabled = true
-        
+
         NotificationCenter
             .default
             .addObserver(self,
@@ -16,30 +16,30 @@ class RAExternalPlaybackAvailabilityContext: RAEventEmitter {
                          name: NSNotification.Name.AVRouteDetectorMultipleRoutesDetectedDidChange,
                          object: nil)
     }
-    
+
     override func stopObserving() {
         super.stopObserving()
         self.avRouteDetector.isRouteDetectionEnabled = false
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     override func supportedEvents() -> [String]! {
         return [RAEvent.externalPlaybackAvailabilityChanged.rawValue]
     }
-    
+
     override class func requiresMainQueueSetup() -> Bool {
         return false
     }
-    
+
     @objc(fetchExternalPlaybackAvailability:withRejecter:)
-    func fetchExternalPlaybackAvailability(_ resolve: RCTPromiseResolveBlock, withRejecter rejecter: RCTPromiseRejectBlock) -> Void {
+    func fetchExternalPlaybackAvailability(_ resolve: RCTPromiseResolveBlock, withRejecter reject: RCTPromiseRejectBlock) {
         resolve(self.getExternalPlaybackAvailability())
     }
-    
+
     private func getExternalPlaybackAvailability() -> Bool {
         return self.avRouteDetector.multipleRoutesDetected
     }
-    
+
     @objc private func handleMultipleRoutesDetectedDidChange() {
         self.sendEvent(withName: RAEvent.externalPlaybackAvailabilityChanged.rawValue, body: self.avRouteDetector.multipleRoutesDetected)
     }
