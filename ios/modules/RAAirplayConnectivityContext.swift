73eb5e51e8594d8ddc244901b8bb76bf2300340e
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-03-04 20:00:56 +1300

    Cleanup new lines

--- ios/modules/RAAirplayConnectivityContext.swift
+++ ios/modules/RAAirplayConnectivityContext.swift
@@ -4,7 +4,7 @@ import AVKit
 class RAAirplayConnectivityContext: RAEventEmitter {
     override func startObserving() {
         super.startObserving()
-
+        
         NotificationCenter
             .default
             .addObserver(self,
@@ -12,32 +12,32 @@ class RAAirplayConnectivityContext: RAEventEmitter {
                          name: AVAudioSession.routeChangeNotification,
                          object: nil)
     }
-
+    
     override func stopObserving() {
         super.stopObserving()
         NotificationCenter.default.removeObserver(self)
     }
-
+    
     override func supportedEvents() -> [String]! {
         return [RAEvent.airplayConnectivityChanged.rawValue]
     }
-
+    
     override class func requiresMainQueueSetup() -> Bool {
         return false
     }
-
+    
     @objc(fetchAirplayConnectivity:withRejecter:)
     func fetchAirplayConnectivity(_ resolve: RCTPromiseResolveBlock, withRejecter reject: RCTPromiseRejectBlock) {
         resolve(self.getAirplayConnectivity())
     }
-
+    
     private func getAirplayConnectivity() -> Bool {
         let session = AVAudioSession.sharedInstance()
         let airplayConnected = session.currentRoute.outputs.contains { $0.portType == .airPlay }
-
+        
         return airplayConnected
     }
-
+    
     @objc private func handleRouteChange() {
         self.sendEvent(withName: RAEvent.airplayConnectivityChanged.rawValue, body: self.getAirplayConnectivity())
     }
