8d7d6f23864adea4f1c01c35701f1418c099a98b
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-07-01 10:09:44 +1200

    fix: minor lint warnings

--- ios/ReactAirplay/Modules/RAEventEmitter.swift
+++ ios/ReactAirplay/Modules/RAEventEmitter.swift
@@ -1,21 +1,21 @@
 class RAEventEmitter: RCTEventEmitter {
     internal var hasListeners = false
-    
+
     override func startObserving() {
         super.startObserving()
         self.hasListeners = true
     }
-    
+
     override func stopObserving() {
         super.stopObserving()
         self.hasListeners = false
     }
-    
+
     override func sendEvent(withName name: String!, body: Any!) {
         guard hasListeners else { return }
         super.sendEvent(withName: name, body: body)
     }
-    
+
     override func supportedEvents() -> [String]! {
         return []
     }
