7ceff9f78e42a95993b9f40b8115c00b8d1f57ad
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-11-28 23:36:55 +1300

    fix: null error on un-supported platforms

--- src/airplayModule.ts
+++ src/airplayModule.ts
@@ -1,11 +1,20 @@
-import {NativeEventEmitter, NativeModules, Platform} from 'react-native';
+import {
+  NativeEventEmitter,
+  NativeModule,
+  NativeModules,
+  Platform,
+} from 'react-native';
 
-export type ExternalPlaybackAvailabilityContext = {
+export type ExternalPlaybackAvailabilityContext = NativeModule & {
   fetchExternalPlaybackAvailability: () => Promise<boolean>;
 };
 
-export type AirplayConnectivityContext = {
+export type AirplayConnectivityContext = NativeModule & {
   fetchAirplayConnectivity: () => Promise<boolean>;
+};
+
+export type RoutePickerContext = NativeModule & {
+  showRoutePicker: (options?: ShowRoutePickerOptions) => Promise<void>;
 };
 
 export type ShowRoutePickerOptions = {
@@ -17,22 +26,23 @@ const {
   RAAirplayConnectivityContext,
   RAExternalPlaybackAvailabilityContext,
   RARoutePickerContext,
-} = NativeModules;
+} = NativeModules as {
+  RAEvents?: {getConstants: () => Record<string, string>};
+  RAAirplayConnectivityContext?: AirplayConnectivityContext;
+  RAExternalPlaybackAvailabilityContext?: ExternalPlaybackAvailabilityContext;
+  RARoutePickerContext?: RoutePickerContext;
+};
 
-const constants =
-  Platform.OS !== 'ios'
-    ? {}
-    : (RAEvents.getConstants() as Record<string, string>);
+const constants = RAEvents?.getConstants();
 
+export const AirplayConnectivityContext = RAAirplayConnectivityContext;
 export const ExternalPlaybackAvailabilityContext =
-  RAExternalPlaybackAvailabilityContext as ExternalPlaybackAvailabilityContext;
-export const AirplayConnectivityContext =
-  RAAirplayConnectivityContext as AirplayConnectivityContext;
+  RAExternalPlaybackAvailabilityContext;
 
 export const {
   EXTERNAL_PLAYBACK_AVAILABILITY_CHANGED,
   AIRPLAY_CONNECTIVITY_CHANGED,
-} = constants;
+} = constants ?? {};
 
 export const ExternalPlaybackAvailabilityEventEmitter = new NativeEventEmitter(
   RAExternalPlaybackAvailabilityContext,
@@ -58,6 +68,10 @@ export const onAirplayConnectivityChanged = (
     callback,
   );
 
-export const showRoutePicker: (
-  options?: ShowRoutePickerOptions,
-) => Promise<void> = RARoutePickerContext.showRoutePicker;
+export const showRoutePicker = (options: ShowRoutePickerOptions) => {
+  if (Platform.OS !== 'ios' && RARoutePickerContext === undefined) {
+    console.warn('showRoutePicker is only supported on iOS');
+  }
+
+  return RARoutePickerContext?.showRoutePicker(options) ?? Promise.resolve();
+};
